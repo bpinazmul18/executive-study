c70c2b603a6e9100667f7790e44b09389aedb51c
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-11-29 12:59:06 +0600

    add features section and carousel slider

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.features-slider-wrapper').owlCarousel({
+        loop: true,
+        nav:false,
+        dots: true,
+        items: 4,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:true,
+    });
+});
